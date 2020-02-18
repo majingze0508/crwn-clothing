e612a5d4c49b585e77cb149f4667e5237a7c811b
Jingze Ma <majingze0508@gmail.com>
2020-02-17 19:08:39 -0500

    sing in from input component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 /*const HomePage = props => {
   console.log(props);
@@ -45,9 +47,11 @@ const TopicDetail = props => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
