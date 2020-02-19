13e9668aec3b3f82c890faf352b1786b3da5a7f0
Jingze Ma <majingze0508@gmail.com>
2020-02-18 20:56:14 -0500

    firebase google sigin

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyCBxjn484I83Yt4I_qiDEyROxtKSgtW-50",
-  authDomain: "crwn-db-d4710.firebaseapp.com",
-  databaseURL: "https://crwn-db-d4710.firebaseio.com",
-  projectId: "crwn-db-d4710",
-  storageBucket: "crwn-db-d4710.appspot.com",
-  messagingSenderId: "132763476338",
-  appId: "1:132763476338:web:4096154b7deab6364f7deb",
-  measurementId: "G-S908KWYNC9"
-};
+const config = {};
 
 firebase.initializeApp(config);
 
